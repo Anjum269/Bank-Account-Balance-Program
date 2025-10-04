7bcc8b33e86b8a41bba683d2b53e57187644019a
Anjum <anjumkousar079@gmail.com>
2025-10-04 11:40:44 +0530

    Initial Bank Account Balance Program with deposit

---
+++ bank_account.py
@@ -0,0 +1,10 @@
+# bank_account.py
+
+initial_balance = float(input("Enter Initial Balance: ₹"))
+deposit = float(input("Enter Deposit Amount: ₹"))
+
+new_balance = initial_balance + deposit
+
+print(f"Initial Balance: ₹{initial_balance}")
+print(f"Deposit: ₹{deposit}")
+print(f"New Balance after deposit: ₹{new_balance}")
